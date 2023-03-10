76816864c3dfb3b87dca3a4c25cc6738b8c7afbb
Gabriel Melo <gabrielmvalley@gmail.com>
2023-03-10 16:33:54 -0300

    Adding controller of route POST

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -18,12 +18,30 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
-    // Criar um registro
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'Email already exists' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   update() {
-    // Editar um registro
+
   }
 
   async delete(request, response) {
