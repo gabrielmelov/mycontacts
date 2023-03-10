e5bd962bb6ae96bd84758cd828794dd666f2836c
Gabriel Melo <gabrielmvalley@gmail.com>
2023-03-10 13:05:22 -0300

    Create Contacts Repository

---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,41 @@
+const { v4 } = require('uuid');
+
+let contacts = [
+  {
+    id: v4(),
+    name: 'Gabriel',
+    email: 'gabriel@email.com',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Jose',
+    email: 'jose@email.com',
+    category_id: v4(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(
+        contacts.find((contact) => contact.id === id),
+      );
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
